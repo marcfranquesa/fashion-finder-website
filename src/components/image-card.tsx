05a90a6c713024db232e01c7a62fb49c26588c69
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-05-04 21:06:20 +0200

    chore: tailwind

--- src/components/image-card.tsx
+++ src/components/image-card.tsx
@@ -1,41 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Image from "next/image";
 
 export function ImageCard({ path }: { path: string }) {
   const [isHovered, setIsHovered] = useState(false);
 
-  
   return (
     <div className="relative w-full h-full">
-        <style jsx>{`
-          .hover-scale {
-            transition: transform 0.3s ease-in-out;
-            z-index: 1;
-          }
-          .hover-scale:hover {
-            transform: scale(1.2);
-          }
-          .inner-container {
-            padding: 0.7rem; /* Adjust the padding as needed */
-          }
-        `}</style>
+      <div className="transition-transform duration-300 ease-in-out z-10 absolute inset-0 hover:scale-125">
         <div
-          className={`absolute inset-0 ${isHovered ? 'hover-scale' : ''}`}
+          className={`inner-container p-4 ${isHovered ? "hover-scale" : ""}`}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
-          <div className="inner-container">
-            <Image
-              src={path}
-              width={200}
-              height={300}
-              alt={path}
-              layout="responsive"
-              objectFit="cover"
-              className="rounded-lg shadow-md"
-            />
-          </div>
+          <Image
+            src={path}
+            width={200}
+            height={300}
+            alt={path}
+            layout="responsive"
+            objectFit="cover"
+            className="rounded-lg shadow-md"
+          />
         </div>
+      </div>
     </div>
   );
 }
