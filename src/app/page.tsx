a74543f03b97b02126818e81e0262eceaea224be
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-05-04 02:27:32 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-4xl font-bold">
+        Welcome to the Inditex Similarity Search
+      </h1>
+    </main>
+  );
+}
