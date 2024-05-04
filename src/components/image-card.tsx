1cb9f44249bbd4a5d276fcdfe3b3308dd8d2938d
Mauro Filomeno Rivero <78371878+maurofr@users.noreply.github.com>
2024-05-04 21:06:59 +0200

    ui: card components
    * Visual improvements to the image boxes and the white spacing below the bar, removed first image as well
    
    * chore: tailwind
    
    ---------
    
    Co-authored-by: Marc Franquesa Monés <marc.franquesam@gmail.com>

--- src/components/image-card.tsx
+++ src/components/image-card.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import Image from "next/image";
 
 export function ImageCard({ path }: { path: string }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="px-4">
-      <Image
-        src={path}
-        width={200}
-        height={300}
-        alt={path}
-        className="border border-black"
-      />
+    <div className="relative w-full h-full">
+      <div className="transition-transform duration-300 ease-in-out z-10 absolute inset-0 hover:scale-125">
+        <div
+          className={`inner-container p-4 ${isHovered ? "hover-scale" : ""}`}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <Image
+            src={path}
+            width={200}
+            height={300}
+            alt={path}
+            layout="responsive"
+            objectFit="cover"
+            className="rounded-lg shadow-md"
+          />
+        </div>
+      </div>
     </div>
   );
 }
